dd3761b9d0d7ca285e75dd6f7d4c4bf6c153e673
jack seymour <stilljack@gmail.com>
2019-09-22 15:52:23 -0700

    type converter installed, alternate, likely much lighter weight alternative commented out -- need to set this as a todo.

--- app/src/main/java/com/saucefan/stuff/foodiefunbw/Model/FoodieEntry.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/Model/FoodieEntry.kt
@@ -2,6 +2,11 @@ package com.saucefan.stuff.foodiefunbw.Model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+
 
 
 @Entity
@@ -11,27 +16,54 @@ data class FoodieEntry(
     val id:Int,
     //presumedly we have a similar primary key that corressponds to a url or an index number or something
     //on the back end
-    val remoteIndex: String,
+        val remoteIndex: String,
     //yep from 1970 or whatever
-    val entryTime: Long,
+        val entryTime: Long,
 
     //actual stuff
-    var foodEaten:String = "blank",
-    var entryText:String = "blank",
-    var rating:Int =1,
-    var uhSomethingElse:String ="blank",
+        var foodEaten:String = "blank",
+        var entryText:String = "blank",
+        var rating:Int =1,
+        var uhSomethingElse:String ="blank",
 
     //so this will be the list of assosiated Uris for images, List<String> can be passed into room without trouble, but wouldn't be hard to make this... like an adjacent table or something
     //will have to be nullable
-    var imageLocal:List<String>? = null,
+       var imageLocal:List<String>? = null,
 
     //isDeleted will indicate to send a message to the server at next sync to delete remote copy
-    var isDeleted:Boolean=false,
+        var isDeleted:Boolean=false,
         //isUpdated is similar, update copy stored server side
-    var isUpdated:Boolean=false
-
-
-
+        var isUpdated:Boolean=false
 
 
 )
+class Converters {
+
+    @TypeConverter
+    fun listToJson(value: List<String>?): String {
+
+        return Gson().toJson(value)
+    }
+
+    @TypeConverter
+    fun jsonToList(value: String): List<String>? {
+
+        val objects = Gson().fromJson(value, Array<String>::class.java) as Array<String>
+        val list = objects.toList()
+        return list
+    }
+}
+/*
+
+lighter weight alternative, consider launching a singleton of gson if necessary for above.
+class StringListConverter {
+    @TypeConverter
+    fun fromString(stringListString: String): List<String> {
+        return stringListString.split(",").map { it }
+    }
+
+    @TypeConverter
+    fun toString(stringList: List<String>): String {
+        return stringList.joinToString(separator = ",")
+    }
+}*/
--- app/src/main/java/com/saucefan/stuff/foodiefunbw/Model/room/RoomDatabase.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/Model/room/RoomDatabase.kt
@@ -2,9 +2,14 @@ package com.saucefan.stuff.foodiefunbw.Model.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.saucefan.stuff.foodiefunbw.Model.Converters
 import com.saucefan.stuff.foodiefunbw.Model.FoodieEntry
 
 @Database(entities = [FoodieEntry::class], version = 2, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class EntryDatabase : RoomDatabase() {
     abstract fun RoomDao(): RoomDao
 }
+
+
