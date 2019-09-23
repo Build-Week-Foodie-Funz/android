77d82f6e09c56258eee6b110236c0d4f3a63d5a6
ronniedipple <ronniedipple@gmail.com>
2019-09-23 00:41:02 -0500

    Added Registration Activity and filled in xml, login activity working ish

--- app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/LoginActivity.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.saucefan.stuff.foodiefunbw.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,7 +16,12 @@ class LoginActivity : AppCompatActivity() { //Ronnie changed xml name to activit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.saucefan.stuff.foodiefunbw.R.layout.activity_login)
-        //just rotates the wave effect
+
+        textView_signup.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java))
+            finish()
+        }
+
 
 
 
