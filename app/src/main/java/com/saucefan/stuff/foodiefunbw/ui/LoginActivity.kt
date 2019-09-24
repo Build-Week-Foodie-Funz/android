fa9bee4bd84116742eb7b1ecc7222656b762c071
ronniedipple <ronniedipple@gmail.com>
2019-09-24 01:43:55 -0500

    Login working, userdatabase setup, no keystore yet

--- app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/LoginActivity.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/LoginActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.navigation.ActivityNavigator
+import com.saucefan.stuff.foodiefunbw.DataBase.UserDatabaseHelper
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_registration.*
 
 
 class LoginActivity : AppCompatActivity() { //Ronnie changed xml name to activity login
@@ -13,17 +17,69 @@ class LoginActivity : AppCompatActivity() { //Ronnie changed xml name to activit
     //if auth token, skip login
 
     //else login, on true redirect to dashboard
+
+    lateinit var handler : UserDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.saucefan.stuff.foodiefunbw.R.layout.activity_login)
 
+        handler = UserDatabaseHelper(this)
+
+        showHome()
+
+
         textView_signup.setOnClickListener {
-            startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java))
-            finish()
+            showRegistration()
+
+        }
+
+        textView_signIn.setOnClickListener {
+            showLogIn()
+        }
+
+        button_save.setOnClickListener {
+            handler.insertUserData(name.text.toString(), password.text.toString())
+            showHome()
+
+        }
+
+        button_signin.setOnClickListener {
+           if(handler.userInputCheck(login_name.text.toString(), login_password.text.toString())){
+               startActivity(Intent(this, DashboardActivity::class.java))
+               finish()
+
+           } else{
+               Toast.makeText(this, "Username or password is incorrect", Toast.LENGTH_SHORT).show()
+           }
+
         }
 
 
 
+
+
+
+
+    }
+
+    private fun showRegistration(){
+
+        //Shows registration layout
+        //Hides login layout
+        registration_layout.visibility = View.VISIBLE
+        home_ll.visibility = View.GONE
+
+    }
+
+    private fun showLogIn(){
+        registration_layout.visibility = View.GONE
+        login_layout.visibility = View.VISIBLE
+    }
+
+    private fun showHome(){
+        registration_layout.visibility = View.GONE
+        login_layout.visibility = View.VISIBLE
 
     }
 }
