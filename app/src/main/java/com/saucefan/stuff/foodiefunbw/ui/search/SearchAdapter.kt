d7493a7ba310581ecb33b126f1c537bdfe17bf5f
ronniedipple <ronniedipple@gmail.com>
2019-09-25 22:44:50 -0500

    all about the adapters really

--- app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/search/SearchAdapter.kt
@@ -1,6 +1,7 @@
 package com.saucefan.stuff.foodiefunbw.ui.search
 
 import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +10,9 @@ import android.widget.TextView
 import com.saucefan.stuff.foodiefunbw.Model.FoodieRestaurant
 import com.saucefan.stuff.foodiefunbw.R
 
-class SearchAdapter(context: Context, val layout: Int, val chapter: List<FoodieRestaurant>) : ArrayAdapter<FoodieRestaurant>(context, layout, chapter) {
+class  SearchAdapter(context: Context, val layout: Int, val chapter: List<FoodieRestaurant>, private val arguments: Bundle?) : ArrayAdapter<FoodieRestaurant>(context, layout, chapter) {
+
+
     override fun getCount(): Int {
         return chapter.size
     }
@@ -19,14 +22,29 @@ class SearchAdapter(context: Context, val layout: Int, val chapter: List<FoodieR
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var retView: View
         var vi = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
         if (convertView == null) {
             retView = vi.inflate(layout, null)
         } else {
             retView = convertView
+
         }
         var chapterItem = getItem(position)
         val chapterName = retView.findViewById(R.id.tvSearch) as TextView
         chapterName.text = chapterItem!!.restName
+        retView.setOnClickListener {
+
+
+
+
+        }
+
+
+
         return retView
+
+
     }
+
+
 }
