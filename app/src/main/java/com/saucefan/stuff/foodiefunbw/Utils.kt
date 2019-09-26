ccc2031747092db31e4659fffba0f9ab20c19ac9
jack seymour <stilljack@gmail.com>
2019-09-25 17:22:55 -0700

    first draft, use with caution

--- app/src/main/java/com/saucefan/stuff/foodiefunbw/Utils.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/Utils.kt
@@ -3,6 +3,8 @@ package com.saucefan.stuff.foodiefunbw
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import com.saucefan.stuff.foodiefunbw.Model.FoodieRestaurant
+import com.saucefan.stuff.foodiefunbw.ui.viewedit.EditRestFrag
+import com.saucefan.stuff.foodiefunbw.ui.viewedit.EditReviewFrag
 import com.saucefan.stuff.foodiefunbw.ui.viewedit.ViewRestFrag
 import com.saucefan.stuff.foodiefunbw.ui.viewedit.ViewReviewFrag
 
@@ -27,10 +29,39 @@ import com.saucefan.stuff.foodiefunbw.ui.viewedit.ViewReviewFrag
 // container where they should appear,
 // ID of object you wish to view
 // and context of the activity
+fun editReviewFragment(container:Int,reviewID:Int,context: Context):String {
+
+    val tag = "reviewFrag$reviewID"
+    val reviewFrag = EditReviewFrag.newInstance(1)
+    val ft = (context as AppCompatActivity).supportFragmentManager
+            .beginTransaction()
+            // 2
+            .add(container, reviewFrag, tag)
+            // 3
+            .addToBackStack("review$reviewID") //presumedly re'd name it the id or perhaps "rest$Id"
+            .commit()
+    return tag
+}
+
+fun editRestFrag(container:Int,restID:Int,context:Context):String  {
+    val tag = "restFrag$restID"
+    val restFrag = EditRestFrag.newInstance(1)
+    val ft = (context as AppCompatActivity).supportFragmentManager
+            .beginTransaction()
+            // 2
+            .add(container, restFrag, tag)
+            // 3
+            .addToBackStack("rest$restID") //presumedly re'd name it the id or perhaps "rest$Id"
+            .commit()
+    return tag
+
+}
+
+
 fun displayReviewFragment(container:Int,reviewID:Int,context: Context):String {
 
     val tag = "reviewFrag$reviewID"
-    val reviewFrag: ViewReviewFrag = ViewReviewFrag.newInstance(1)
+    val reviewFrag = ViewReviewFrag.newInstance(1)
     val ft = (context as AppCompatActivity).supportFragmentManager
             .beginTransaction()
             // 2
