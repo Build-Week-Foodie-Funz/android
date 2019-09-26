1ea3300402177702b60b5946f139a69ca2042ace
jack seymour <stilljack@gmail.com>
2019-09-26 10:43:28 -0700

    9/26/2019 10:40 edit rest&review frags initial work, todo: handle antipated issues create data sanitization methods, call anticipated methods

--- app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/viewedit/fragmentDisplayActivityDeleteThis.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/viewedit/fragmentDisplayActivityDeleteThis.kt
@@ -5,7 +5,10 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.saucefan.stuff.foodiefunbw.*
+import kotlinx.android.synthetic.main.activity_fragment_display_delete_this.*
 import kotlinx.android.synthetic.main.activity_prefs.*
+import kotlinx.android.synthetic.main.activity_prefs.btn_rest
+import kotlinx.android.synthetic.main.activity_prefs.btn_rev
 
 class fragmentDisplayActivityDeleteThis : AppCompatActivity(),ViewRestFrag.ViewRestFragmentListener, ViewReviewFrag.ViewReviewFragmentListener {
     override fun onFragmentInteraction(uri: Uri) {
@@ -16,10 +19,10 @@ class fragmentDisplayActivityDeleteThis : AppCompatActivity(),ViewRestFrag.ViewR
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment_display_delete_this)
         btn_rest.setOnClickListener {
-            editRestFrag(R.id.fragment_container_view_tag,2,this)
+            editRestFrag(R.id.fragment_container_view_tag,et.text.toString().toInt(),this)
         }
         btn_rev.setOnClickListener {
-            editReviewFragment(R.id.fragment_container_view_tag,2,this)
+            editReviewFragment(R.id.fragment_container_view_tag,et.text.toString().toInt(),this)
         }
     }
 
