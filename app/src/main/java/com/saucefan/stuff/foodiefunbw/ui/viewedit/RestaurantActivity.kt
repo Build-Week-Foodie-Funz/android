8c4b12ab7b53f74e5b1151dccdcb66a7bd932c81
jack seymour <stilljack@gmail.com>
2019-09-26 17:38:58 -0700

    rebaseneeded

--- app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/viewedit/RestaurantActivity.kt
+++ app/src/main/java/com/saucefan/stuff/foodiefunbw/ui/viewedit/RestaurantActivity.kt
@@ -2,27 +2,33 @@ package com.saucefan.stuff.foodiefunbw.ui.viewedit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.saucefan.stuff.foodiefunbw.R
 import kotlinx.android.synthetic.main.activity_restaurant.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.saucefan.stuff.foodiefunbw.Model.FoodieEntry
 import com.saucefan.stuff.foodiefunbw.Model.FoodieRestaurant
+import com.saucefan.stuff.foodiefunbw.viewmodel.FoodieEntryViewModel
 import kotlinx.android.synthetic.main.activity_restaurant.recyclerView_rest
 import kotlinx.android.synthetic.main.activity_restaurant.view.*
+import androidx.lifecycle.ViewModelProvider
+import android.R
+
+
 
 class RestaurantActivity : AppCompatActivity() {
     lateinit var layoutManager: LinearLayoutManager
-    private lateinit var viewModel: FoodieRestaurant
+    private lateinit var viewModel: FoodieEntryViewModel
     lateinit var adapter: ViewRestAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_restaurant)
+        setContentView(com.saucefan.stuff.foodiefunbw.R.layout.activity_restaurant)
+        viewModel = ViewModelProvider(this).get(FoodieEntryViewModel::class.java)
 
 
-
-
+setupRecyclerView(viewModel)
         recyclerView_rest.apply {
             layoutManager = LinearLayoutManager(this@RestaurantActivity)
            /* adapter = ViewRestAdapter()*/
@@ -31,7 +37,27 @@ class RestaurantActivity : AppCompatActivity() {
 
 
     }
+    fun setupRecyclerView(viewModel:FoodieEntryViewModel) {
+        recyclerView_rest.layoutManager = LinearLayoutManager(this) // or grid or whatever
+        recyclerView_rest.setHasFixedSize(true) // this actually may need to be false
 
+        var adapter = ViewRestAdapter()
+
+        recyclerView_rest.adapter = adapter
+
+
+        viewModel..observe(this, Observer<List<fOODIEENTRY OR RESTOBJ>> {
+            updateRecyclerView(adapter, it as MutableList<fOODIEENTRY OR RESTOBJ>)
+        })
+
+    }*/
+
+
+
+    fun updateRecyclerView(adapter: ViewRestAdapter, list: MutableList<String>) {
+        adapter.submitList(list as List<String>)
+        adapter.notifyDataSetChanged()
+    }
   /*  viewModel.returnAllItems().observe(this, Observer<List<FoodieEntry>> {
         updateRecyclerView(adapter, it as MutableList<FoodieEntry>)
     })
